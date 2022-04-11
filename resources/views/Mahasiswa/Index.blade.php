a73821112d45aa77f20e244b8ab27420fa47e5da
safira123-pixel <safiraistifarini@gmail.com>
2022-04-11 09:13:34 +0700

    tugas praktikum 7

--- resources/views/Mahasiswa/Index.blade.php
+++ resources/views/Mahasiswa/Index.blade.php
@@ -1,26 +1,25 @@
-@extends('mahasiswa.layout')
+extends('mahasiswa.layout')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left mt-2">
-                <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
-            </div>
-            <div class="float-right my-2">
-            <a class="btn btn-success" href="{{ route('mahasiswa.create') }}"> Input Mahasiswa</a>
-            </div>
-        </div>
+<div class="row">
+  <div class="col-lg-12 margin-tb">
+    <div class="pull-left mt-2">
+      <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
     </div>
-
+    <div class="float-right my-2">
+      <a class="btn btn-success" href="{{ route('mahasiswa.create') }}"> Input Mahasiswa</a>
+    </div>
+  </div>
+</div>
 @if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
+<div class="alert alert-success">
+  <p>{{ $message }}</p>
+</div>
 @endif
 @if ($message = Session::get('error'))
-    <div class="alert alert-error">
-        <p>{{ $message }}</p>
-    </div>
+<div class="alert alert-error">
+  <p>{{ $message }}</p>
+</div>
 @endif
 
 <form class="form" method="get" action="/search">
@@ -32,49 +31,44 @@
 </form>
 
 <table class="table table-bordered">
-    <tr>
-        <th>Nim</th>
-        <th>Nama</th>
-        <th>Kelas</th>
-        <th>Jurusan</th>
-        <th>Email</th>
-        <th>Alamat</th>
-        <th>Tanggal Lahir</th>
-        <th width="280px">Action</th>
-    </tr>
-    @if(!empty($data) && $data->count())
+  <tr>
+    <th>Nim</th>
+    <th>Nama</th>
+    <th>Kelas</th>
+    <th>Jurusan</th>
+    <th>Email</th>
+    <th>Alamat</th>
+    <th>Tanggal Lahir</th>
+    <th width="280px">Action</th>
+  </tr>
+  @if(!empty($data) && $data->count())
     @foreach($data as $mhs)
-    <tr>
-
-        <td>{{ $mhs ->nim }}</td>
-        <td>{{ $mhs ->nama }}</td>
-        <td>{{ $mhs ->kelas }}</td>
-        <td>{{ $mhs ->jurusan }}</td>
+      <tr>
+        <td>{{ $mhs->nim }}</td>
+        <td>{{ $mhs->nama }}</td>
+        <td>{{ $mhs->kelas }}</td>
+        <td>{{ $mhs->jurusan }}</td>
         <td>{{ $mhs->email }}</td>
         <td>{{ $mhs->alamat }}</td>
         <td>{{ $mhs->tanggallahir }}</td>
         <td>
-            <form action="{{ route('mahasiswa.destroy',['mahasiswa'=>$mhs->nim]) }}" method="POST">
-
-                <a class="btn btn-info" href="{{ route('mahasiswa.show',$mhs->nim) }}">Show</a>
-
-                <a class="btn btn-primary" href="{{ route('mahasiswa.edit',$mhs->nim) }}">Edit</a>
-
-                @csrf
-                @method('DELETE')
-
-                <button type="submit" class="btn btn-danger">Delete</button>
-            </form>
+          <form action="{{ route('mahasiswa.destroy',['mahasiswa'=>$mhs->nim]) }}" method="POST">
+            <a class="btn btn-info" href="{{ route('mahasiswa.show',$mhs->nim) }}">Show</a>
+            <a class="btn btn-primary" href="{{ route('mahasiswa.edit',$mhs->nim) }}">Edit</a>
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">Delete</button>
+          </form>
         </td>
-    </tr>
-    
-@endforeach
-@else
+      </tr>
+    @endforeach
+  @else
     <tr>
       <td colspan="10">There are no data.</td>
     </tr>
   @endif
 </table>
 
+{!! $data->links() !!}
 
 @endsection
